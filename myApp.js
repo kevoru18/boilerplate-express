d8161ff4cf75db971df18afcc82570c668a3d405
kevoru18 <kevoru@gmail.com>
2022-06-16 13:37:31 -0600

    otra

--- myApp.js
+++ myApp.js
@@ -3,12 +3,12 @@
 require("dotenv").config()
 let express = require('express');
 let app = express();
-// absolutePath=__dirname + "/views/index.html";
-// ruta=__dirname+"/public"
+absolutePath=__dirname + "/views/index.html";
+ruta=__dirname+"/public"
 console.log("Hello World");
-// app.get("/",(req,res)=>{
-//     res.sendFile(absolutePath)
-// })
+app.get("/",(req,res)=>{
+    res.sendFile(absolutePath)
+})
 app.use("/public",express.static(ruta))
 app.get('/json',(function(req,res){
    
